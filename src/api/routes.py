7687af0efe4574e27592c7c45c92fb48f8ccb469
tali-tali-web <recovertalephgaming@gmail.com>
2026-06-12 10:38:56 +0000

    i hate my life my laptop broke.
    Optimization of file structures

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
 
+router = FastAPI()
+
+@router.post("/data")
+def first_example():
+    return {"message" : "Hello World! "}
+
+@router.get("/dashboard")
+def request_dashboard():
+    return{"tree" : "yes"}
