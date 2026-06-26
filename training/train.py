d047ca9c20480df659f70214fbc973589edc5514
tali-tali-web <recovertalephgaming@gmail.com>
2026-06-26 16:57:38 +0800

    fsdf

--- training/train.py
+++ training/train.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import joblib
 
+#did it work
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
